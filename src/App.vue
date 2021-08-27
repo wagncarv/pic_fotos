adb4d14bbbb4ab49d3e440f16c7280b97f8c139b
Wagner Carvalho <wagncarv@gmail.com>
2021-08-27 14:27:44 -0300

    validacao props, type e required props, app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="corpo">
 
-    <nav>
-      <ul>
-        <li v-for="route in routes">
-          <router-link :to="route.path ? route.path : '/'">
-            {{ route.titulo }}
-          </router-link>
-        </li>
-      </ul>
-    </nav>
-
+    <meu-menu :rotas="routes"/>
+ 
     <transition name="pagina">
       <router-view></router-view>
     </transition>
-    
     
   </div>
 </template>
 
 <script>
 import { routes } from './routes';
+import Menu  from './components/shared/menu/Menu.vue';
 
 export default {
+  components: {
+    'meu-menu': Menu
+  },
  data(){
    return {
      routes
