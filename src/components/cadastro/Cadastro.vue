e8d326676df5748ca9c5c1a49f88fafb03cdd939
Wagner Carvalho <wagncarv@gmail.com>
2021-08-28 20:44:30 -0300

    v-model, tw way data bind

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,14 +1,65 @@
 <template>
-    <h2 class="centralizado">Cadastro</h2>
+    <div>
+        <h1 class="centralizado">Cadastro</h1>
+        <h2 class="centralizado">{{ foto.titulo }}</h2>
+
+        <form @submit.prevent="grava()"> 
+            <div class="controle">
+                <label for="titulo">Título</label>
+                <input id="titulo" autocomplete="off" v-model="foto.titulo"/>
+            </div>
+
+            <div class="controle">
+                <label for="url">URL</label>
+                <input id="url" autocomplete="off" v-model="foto.url" />
+                <imagem-responsiva />
+            </div>
+
+            <div class="controle">
+                <label for="descricao">Descrição</label>
+                <textarea autocomplete="off" id="descricao" v-model="foto.descricao"></textarea>
+            </div>
+
+            <div class="centralizado">
+                <meu-botao rotulo="Gravar" tipo="submit" />
+                <router-link to="/">
+                    <meu-botao rotulo="Voltar" tipo="button" />
+                </router-link>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
 
 export default {
-    setup() {
+    data(){
         return {
-            message: "olá"
+            foto: {
+                titulo: '',
+                url: '',
+                descricao: ''
+            }
         }
+    },
+    methods: {
+        grava(){
+            console.log('Enviar dados para API');
+            this.limpa();
+        },
+        limpa(){
+            this.foto = {
+                titulo: '',
+                url: '',
+                descricao: ''
+            }
+        }
+    },
+    components: { 
+      'imagem-responsiva ': ImagemResponsiva,
+      'meu-botao': Botao
     },
 }
 </script>
@@ -16,5 +67,21 @@ export default {
 <style scoped>
     .centralizado {
         text-align: center;
+    }
+
+    .controle {
+        font-size: 1.2em;
+        margin-bottom: 20px;
+    }
+
+    .controle label {
+        display: block;
+       font-weight: bold;
+    }
+
+    .controle label + input, .controle textarea {
+        width: 100%;
+       font-size: inherit;
+       border-radius: 5px;
     }
 </style>
