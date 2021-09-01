0909187c3400d51c032c6951b08f958de5ad2232
Wagner Carvalho <wagncarv@gmail.com>
2021-09-01 15:49:54 -0300

    inclusao vee validate

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
 import './directives/Transform';
+import VeeValidate from 'vee-validate';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
@@ -14,6 +15,8 @@ const router = new VueRouter({
   routes,
   mode: 'history'
 })
+
+Vue.use(VeeValidate)
 
 new Vue({
   el: '#app',
