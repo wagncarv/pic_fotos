a714bdbf32326a964cc6cba04d54829d1e3da2e4
Wagner Carvalho <wagncarv@gmail.com>
2021-09-04 14:06:01 -0300

    config linguagem aviso, validacao, vee validate, pt_BR

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import VueRouter from 'vue-router';
 import { routes } from './routes';
 import './directives/Transform';
 import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
@@ -16,7 +17,14 @@ const router = new VueRouter({
   mode: 'history'
 })
 
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, {
+  locale: 'pt_BR',
+  dictionary: {
+    pt_BR: {
+      messages: msg
+    }
+  }
+});
 
 new Vue({
   el: '#app',
